a553bcee9f6d7a57ee82afe26ed696e48fc3aad8
May Guo <40670609+windcloak@users.noreply.github.com>
2021-01-18 16:28:08 -0600

    Added fixed update

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     rb.AddForce(0, 200, 500);
+    // }
+    // We marked this as FixedUpdate b/c we are using it to mess with physics
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); // add force of 2000 on z-axis
+    }
+}
