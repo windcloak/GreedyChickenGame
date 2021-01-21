4d621306eac2079a7ec744dc5e4c6f42a6f0bee9
May Guo <40670609+windcloak@users.noreply.github.com>
2021-01-20 22:32:48 -0600

    Added win scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
     public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
     public void EndGame()
     {
         if (!gameHasEnded)
