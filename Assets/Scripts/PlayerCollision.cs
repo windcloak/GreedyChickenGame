8459ed77c514cb9e442cc7574f8049b923db4a2d
May Guo <40670609+windcloak@users.noreply.github.com>
2021-01-22 00:34:02 -0600

    imported assets, added yellow candy

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,11 +4,19 @@ public class PlayerCollision : MonoBehaviour
 {
 
     public PlayerMovement movement;
+    public GameObject eatCandyEffect;
 
     void OnCollisionEnter(Collision collisionInfo) {
         if (collisionInfo.collider.name == "Obstacle") {
-            movement.enabled = false;
-            FindObjectOfType<GameManager>().EndGame();
+            //movement.enabled = false;
+            //FindObjectOfType<GameManager>().EndGame();
+            EatCandy();
         }
+    }
+
+    void EatCandy()
+    {
+        Debug.Log("Got candy");
+        // increase score                            
     }
 }
---
+++ Assets/YellowCandy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class YellowCandy : MonoBehaviour
+{
+    public GameObject eatenEffect;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Player")
+        {
+            CandyEaten();
+        }
+    }
+     
+    void CandyEaten()
+    {
+        Instantiate(eatenEffect, transform.position, transform.rotation);   // show effect
+        Destroy(gameObject);    // destroy candy
+    }
+}
