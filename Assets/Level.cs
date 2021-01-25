614144163d90592f92283a15bfb574d684c8f780
May Guo <40670609+windcloak@users.noreply.github.com>
2021-01-24 21:48:46 -0600

    added display of level at end

---
+++ Assets/Level.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public Text currentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel.text = "LEVEL " + SceneManager.GetActiveScene().buildIndex.ToString();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //    currentLevel.text = "LEVEL " + SceneManager.GetActiveScene().buildIndex.ToString();
+    //}
+}
