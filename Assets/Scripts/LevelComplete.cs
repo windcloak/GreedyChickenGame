e2caf7de27a00373e1307084659077c7fc910aa1
May Guo <40670609+windcloak@users.noreply.github.com>
2021-01-26 20:39:09 -0600

    Changed title to Bocah font

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -3,15 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
-    private void Start()
-    {
-        // checks if we are at last level
-        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
-        {
-            LoadNextLevel();    // load credits scene
-            Debug.Log("we at last level");
-        }
-    }
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
