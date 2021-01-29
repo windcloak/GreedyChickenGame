0c78b1e12f95c89bfed260a08ddb86d334772d42
May Guo <40670609+windcloak@users.noreply.github.com>
2021-01-28 23:22:30 -0600

    Add bomb

---
+++ Assets/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject hitEffect;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Player")
+        {
+            HitPlayer();
+        }
+
+    }
+
+    void HitPlayer()
+    {
+        FindObjectOfType<AudioManager>().Play("Bomb");
+        Instantiate(hitEffect, transform.position, transform.rotation);   // show effect
+    }
+}
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -5,21 +5,31 @@ public class PlayerCollision : MonoBehaviour
 
     public PlayerMovement movement;
     public float foxCollision = 30f;
+    public float bombCollision = 50f;
 
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Fox")
         {
-            if (Score.score - foxCollision < 0)
-            {
-                PlayerMovement.ResetPlayer();
-            } else
-            {
-
-            Score.score -= foxCollision;
-            }
+            HitPenalty(foxCollision);
+        }
+        if (collisionInfo.collider.tag == "Bomb")
+        {
+            HitPenalty(bombCollision);
         }
 
+    }
+
+    void HitPenalty(float pointLoss)
+    {
+        if (Score.score - pointLoss < 0)
+        {
+            PlayerMovement.ResetPlayer();
+        }
+        else
+        {
+            Score.score -= pointLoss;
+        }
     }
 
 }
